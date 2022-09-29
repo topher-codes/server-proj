76467389a707e9c56c98957ec54ad9e86e6c1441
Chris <ghost@drednaut.net>
2022-09-29 10:20:17 -0700

    completed search frontend
    Signed-off-by: Chris <ghost@drednaut.net>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [animals, setAnimals] = useState([]);
+
+	const search = async (q) => {
+		fetch(`http://localhost:8080?q=${q}`);
+		const response = await fetch(`http://localhost:8080?q=${q}`);
+		const data = await response.json();
+		setAnimals(data);
+	};
+
+	return (
+		<main>
+			<h1>Animal Farm</h1>
+
+			<input
+				type="text"
+				placeholder="Search for animals"
+				onChange={(e) => {
+					const query = e.target.value;
+					search(query);
+				}}
+			/>
+
+			<ul>
+				{animals.map((animal) => (
+					<Animal key={animal.id} {...animal} />
+				))}
+
+				{animals.length === 0 && <li>No animals found</li>}
+			</ul>
+		</main>
+	);
+}
+function Animal({ type, name, age }) {
+	return (
+		<li>
+			{name} is a {age} year old {type}
+		</li>
+	);
 }
 
 export default App;
